048210060a8b0fec2dddf49b1cd7e8d4fe884b6c
orechen422dazo <jboy422hobby@gmail.com>
2024-10-19 23:20:46 +0900

    style: discription

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { ToastComponent } from "./components/toast/ToastComponent";
 export default function Home() {
   return (
     <div>
+      <h1>🍒Cherry</h1>
+      <p>これはさくらんぼです</p>
       <ButtonComponent/>
       <ToastComponent/>
     </div>
