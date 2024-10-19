e0a91f713a13a34c5837b9b575bb95c562c788b2
orechen422dazo <jboy422hobby@gmail.com>
2024-10-19 22:24:32 +0900

    edit: text

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>hoge111</h1>
-      <h2>hoge2</h2>
-      <h2>hoge2</h2>
+      <h1>Hi ya</h1>
+      <p>ffffffffffff</p>
     </div>
   );
 }
